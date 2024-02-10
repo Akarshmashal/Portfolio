6f37c43c699f64e5d2f3a9186854396b57b6f5d1
Akarshmashal <mashalakarsh@gmail.com>
2024-02-10 18:15:34 +0530

    This is my Portfolio it has Home, Skills, Education, Project and contactMe pages

---
+++ script.js
@@ -0,0 +1,23 @@
+// let forms = document.getElementById('form');
+
+// form.addEventListener('submit', function(event) {
+//     event.preventDefault()
+
+//     var fname = document.getElementById('fname').value;
+//     console.log(fname)
+// })
+
+// function myFunction() {
+//     console.log("Nitish")
+//     if(fname === " "){
+//         alert("Please enter the details")
+//     }
+//     else {
+//         alert("Thank You For Contacting US")        
+//     }
+//   }
+
+function submitbtn() {
+    console.log("nitish");
+    alert("Thank You For Contacting US, We will get back you")
+}
